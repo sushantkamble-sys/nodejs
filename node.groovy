249d1b12e1f46b486830dc03b9e3cee268970fad
sushant <sushantkamble949@gmail.com>
2022-05-21 13:22:38 +0530

    update

---
+++ node.groovy
@@ -0,0 +1,20 @@
+pipeline { 
+    agent nodejs 
+    }
+    stages {
+        stage('git-pull') { 
+            steps { 
+                git branch: 'main', credentialsId: '08bf9e1a-a236-48ad-9fe3-254ea8ff0bcf', url: 'https://github.com/miroslavpejic85/mirotalk.git'
+            }
+        }
+        stage('Deploy') {
+            steps {
+                sh '''
+                cd mirotalk
+                cp .env.template .env
+                npm install
+                npm start
+            }
+        }
+    }
+}
